ccc2d6d0f11658c75f95ba2325d68aeb43c63aae
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-08 11:52:09 +0600

    structure ok

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Homepages from './Pages/Homepages';
+import { BrowserRouter } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
+const App = () => {
+  return (
+    <div> 
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Homepages/>} />
+    </Routes>
+    </BrowserRouter>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Pages/Homepages.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../Components/Layout';
+
+const Homepages = () => {
+    return (
+       <Layout>
+        <p className='w-full h-screen flex flex-col justify-center items-center'>This is home page.</p>
+       </Layout>
+    );
+};
+
+export default Homepages;
