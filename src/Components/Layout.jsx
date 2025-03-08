a31f352a1a088a72b14e6a2acabc0957a6d00098
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-08 12:15:23 +0600

    resorce path edit

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -3,16 +3,16 @@ import React from 'react';
 const Layout = (props) => {
     return (
         <div className='w-full bg-gradient-to-br from-green-200 from-10% via-sky-200 via-30% to-orange-100 to-90%'>
-       <img src="/src/resource/portfolio-svgrepo-com.svg" alt="logo" width={40} height={40} 
+       <img src="./src/resource/portfolio-svgrepo-com.svg" alt="logo" width={40} height={40} 
       className='absolute top-4 left-4 ' />
         <div className='absolute bottom-0 left-0 w-full md:w-fit md:h-full
        md:flex md:flex-col md:justify-center md:items-center'>
         <div className='bg-slate-400 md:bg-transparent flex items-center justify-around md:flex-col py-4 '>
-        <img src="/src/resource/linkedin-round-svgrepo-com.svg" alt="linkedin" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
-      <img src="/src/resource/facebook-svgrepo-com (1).svg" alt="facebook" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0 ' />
-      <img src="/src/resource/gmail-svgrepo-com.svg" alt="gmail" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
-      <img src="/src/resource/instagram-rounded-svgrepo-com.svg" alt="insta" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
-      <img src="/src/resource/github-svgrepo-com.svg" alt="github" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
+        <img src="./src/resource/linkedin-round-svgrepo-com.svg" alt="linkedin" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
+      <img src="./src/resource/facebook-svgrepo-com (1).svg" alt="facebook" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0 ' />
+      <img src="./src/resource/gmail-svgrepo-com.svg" alt="gmail" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
+      <img src="./src/resource/instagram-rounded-svgrepo-com.svg" alt="insta" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
+      <img src="../resource/github-svgrepo-com.svg" alt="github" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
         </div>
         </div>
         {props.children}
