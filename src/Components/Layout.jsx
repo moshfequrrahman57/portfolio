0abb716ceef52b9815c7593a1b2319fc64cd70c7
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-08 16:12:16 +0600

    layout complete

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -4,8 +4,8 @@ const Layout = (props) => {
     return (
         <div className='w-full bg-gradient-to-br from-green-200 from-10% via-sky-200 via-30% to-orange-100 to-90%'>
        <img src="/portfolio-svgrepo-com.svg" alt="logo" width={40} height={40} 
-      className='absolute top-4 left-4 ' />
-        <div className='absolute bottom-0 left-0 w-full md:w-fit md:h-full
+      className='fixed top-4 left-4 ' />
+        <div className='fixed bottom-0 left-0 w-full md:w-fit md:h-full
        md:flex md:flex-col md:justify-center md:items-center'>
         <div className='bg-slate-400 md:bg-transparent flex items-center justify-around md:flex-col py-4 '>
         <img src="/linkedin-round-svgrepo-com.svg" alt="linkedin" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
@@ -14,6 +14,21 @@ const Layout = (props) => {
       <img src="/instagram-rounded-svgrepo-com.svg" alt="insta" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
       <img src="/github-svgrepo-com.svg" alt="github" width={40} height={40} className=' md:my-2 md:ml-4 md:p-0' />
         </div>
+        </div>
+        <div className='fixed top-4 right-4 h-full flex flex-col items-center justify-start 
+        md:h-fit md:flex-row md:w-full md:justify-end
+       '>
+          <div className=' md:flex md:basis-3/4 bg-yellow-200  md:justify-around'>
+          <img src="home-svgrepo-com.svg" alt="Home" width={40} height={40} className='my-2'/>
+          <img src="/public/about-successful-man-svgrepo-com.svg" alt="about" width={40} height={40} className='my-2'/>
+          <img src="/public/education-svgrepo-com.svg" alt="edu" width={40} height={40} className='my-2'/>
+          <img src="/public/skill.svg" alt="skill" width={40} height={40} className='my-2'/>
+          <img src="/public/project-21.svg" alt="projects" width={40} height={40} className='my-2'/>
+          <img src="/public/experience.svg" alt="Experience" width={40} height={40} className='my-2'/>
+          <img src="/public/contact-svgrepo-com.svg" alt="Contact" width={40} height={40} className='my-2'/>
+          <img src="/public/resume-portfolio-svgrepo-com.svg" alt="Resume" width={40} height={40} className='my-2'/>
+          
+          </div>
         </div>
         {props.children}
         </div>
