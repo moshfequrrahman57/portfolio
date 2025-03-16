dc1521a45615e1111f15f6f890038471831acdd1
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-16 08:40:52 +0600

    nav animation com

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -1,8 +1,50 @@
+import { useRef } from "react";
 
 const Layout = (props) => {
+    const nav=useRef();
+     const transparent_div=useRef();
+
+  
     return (
       
-        <div className="m-0 p-0 box-border">
+        <div  className="m-0 p-0 box-border">
+        <header className=" shadow w-full fixed top-0 lef flex items-center justify-between bg-green-500 z-30">
+            <figure className="">
+                <img src="/signature-solid-svgrepo-com.svg" alt="logo" width={50} className="w-14 h-14 p-1"/>
+            </figure>
+            <div className="flex items-center justify-end ">
+                <ul ref={nav} className="absolute md:relative top-14 md:top-0 left-0 m-10 md:m-0 w-[calc(100%-80px)] h-[calc(100vh-56px-80px)] md:w-full md:h-fit
+                rounded-xl shadow-lg md:rounded-none md:shadow-none 
+                flex-col md:flex-row   items-center  bg-yellow-200 justify-around md:justify-end 
+               flex  clip-start md:[clip-path:none] opacity-0 md:opacity-100 
+               transition-all duration-[2000ms] ease-in-out z-40">
+               <li> <a href="#" >Home</a></li>
+                 <li> <a href="#" >Skill</a></li>
+                 <li> <a href="#" >Services</a></li>
+            </ul>
+            <button className="">
+            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1"/></button>
+            <button onClick={()=>{
+                console.log("Button Clicked");
+               nav.current.classList.toggle("clip-end-dup");
+               document.body.classList.toggle("no-scroll");
+               nav.current.classList.toggle("opacity_full");
+
+
+               transparent_div.current.classList.toggle("active_dropdown");
+               let value=document.body.scrollHeight;
+               console.log("Value: ",value);
+               let value_string=value.toString();
+               value_string=value_string.concat("px");
+              transparent_div.current.style.height=value_string;
+             
+              
+            }} className="relative">
+            <img  src="/menu-dots-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1 md:hidden"/></button>
+       
+            </div>
+            </header>
+            <div ref={transparent_div} className="absolute hidden top-0 left-0 w-full h-16  bg-emerald-300 z-10">abcdefgh</div>
         {props.children}
         </div>
      
--- src/Pages/Homepages.jsx
+++ src/Pages/Homepages.jsx
@@ -1,89 +1,12 @@
-import React from 'react';
+
 import Layout from '../Components/Layout';
-import { useRef,useState } from 'react';
-
-
 
 const Homepages = () => {
-    let div_width=300;
-   const [scroll_position,setScroll_position]=useState(0);
-    const scroll_div=useRef();
 
-   const handle_scrolling=(scroll_amount)=>{
-        let newScroolPosition=scroll_position+scroll_amount;
-        if(newScroolPosition<0){newScroolPosition=0;}
-        if(newScroolPosition>2100){newScroolPosition=2100;}
-    setScroll_position(newScroolPosition);
-    scroll_div.current.scrollLeft=newScroolPosition;
-    console.log(newScroolPosition);
-    
-   }
-
-let div_array=[1,2,3,4,5,6,7,8];
-
-//touch event
-let touchstartX,touchstartY,touchendX,touchendY;
-
-const touch_start=(event)=>{
-    touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
-    console.log(touchstartX,touchstartY);
-  }
-  const touch_end=(event)=>{
-    touchendX = event.changedTouches[0].screenX;
-    touchendY = event.changedTouches[0].screenY;
-    console.log(touchendX,touchendY);
-    handle_gesture();
-  }
-
- function handle_gesture(){
-    if (touchendX < touchstartX) {
-      console.log('Swiped Left');
-      handle_scrolling(div_width);
-  }
-
-  if (touchendX > touchstartX) {
-      console.log('Swiped Right');
-      handle_scrolling(-div_width);
-  }
-
-  if (touchendY < touchstartY) {
-      console.log('Swiped Up');
-  }
-
-  if (touchendY > touchstartY) {
-      console.log('Swiped Down');
-  }
-
-  if (touchendY === touchstartY) {
-      console.log('Tap');
-  }
-
-  }
-
+ 
     return (
        <Layout>
-        <div className='bg-red-500 p-8 md:p-10 w-full h-screen  '>
-            <div ref={scroll_div} className='bg-emerald-300 flex justify-start items-center w-full h-2/3 
-            overflow-x-scroll scroll-smooth '>
-                {
-                    div_array.map((item,index)=>{
-                        return(
-                            <div onTouchStart={touch_start} onTouchEnd={touch_end} className='mx-2 bg-yellow-200 flex items-center justify-center font-bold
-                            text-lg h-4/5 w-[300px] flex-shrink-0  '>{item.toString()}</div>
-                        )
-                    })
-                }
-                
-               
-            </div>
-            <div className='flex mt-4 justify-center items-center gap-6'>
-                    <button className='rounded-lg  border p-2 bg-fuchsia-400 font-bold'
-                    onClick={()=>{handle_scrolling(-div_width)}}>Left Scrool</button>
-                    <button className='rounded-lg border p-2 bg-fuchsia-400 font-bold'
-                    onClick={()=>{handle_scrolling(div_width)}}>Right Scroll</button>
-                </div>
-        </div>
+        <p className=' w-full h-[300vh] flex items-center bg-red-200 justify-center text-3xl font-bold'>This is Home page</p>
        </Layout>
     );
 };
