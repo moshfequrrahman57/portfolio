932d0594cb029779e82cf23406e69514d83aaedb
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-17 04:05:15 +0600

    navbar complete

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -13,36 +13,76 @@ const Layout = (props) => {
             <figure className="">
                 <img src="/signature-solid-svgrepo-com.svg" alt="logo" width={50} className="w-14 h-14 p-1"/>
             </figure>
-            <div className="flex md:w-1/2 items-center justify-around">
+            <div className="flex md:w-3/4 items-center justify-around">
                 <ul ref={nav} className="absolute md:relative top-[68px] md:top-0 right-0 
-                md:m-0 w-[calc(100%-80px)] min-h-[calc(100vh-80px)] md:min-h-fit md:w-full md:h-fit
-                rounded-t-lg shadow-sm md:rounded-none md:shadow-none 
+                md:m-0 w-full h-[calc(100vh-80px)] md:w-full md:h-fit
+                rounded-t-lg shadow-lg md:rounded-none md:shadow-none 
                 flex-col md:flex-row   md:items-center  bg-gradient-to-l from-[#FBF9AC] to-slate-300 
-                justify-start md:bg-none 
+                justify-start md:bg-none overflow-auto md:overflow-hidden
                flex  clip-start md:[clip-path:none] 
                transition-all duration-[2000ms] md:duration-0 ease-in-out z-40">
-              <div className="md:hidden w-full flex items-center justify-around">
-                  <button  className="my-3 px-5 py-2 font-semibold text-lg inline-block rounded-lg bg-blue-400 hover:bg-cyan-200 
+              <div className="md:hidden h-fit">
+                <img src="/moshfequr.jpg" alt="moshfequr" className="w-full h-24  rounded-t-lg object-cover  " />
+              </div>
+              <div className="flex flex-col md:flex-row items-center justify-center m-4 md:m-0 border-b-2 border-slate-500 md:border-none">
+                   <a href="#" className="nav_item" >
+                    <img src="/home-svgrepo-com.svg" alt="home" className="md:hidden w-9 h-9 p-1" />
+                    <span className="mr-1 md:px-1">Home</span>
+                   </a>
+                   <a href="#" className="nav_item" >
+                    <img src="/skill.svg" alt="skill" className="md:hidden w-9 h-9 p-1" />
+                    <span className="mr-1 md:px-1">Skill</span>
+                   </a>
+                   <a href="#" className="nav_item" >
+                    <img src="/services.svg" alt="services" className="md:hidden w-9 h-9 p-1" />
+                    <span className="mr-1 md:px-1">Services</span>
+                   </a>
+                   <a href="#" className="nav_item" >
+                    <img src="/project-21.svg" alt="projects" className="md:hidden w-9 h-9 p-1" />
+                    <span className="mr-1 md:px-1">Projects</span>
+                   </a>
+                   <a href="#" className="nav_item" >
+                    <img src="/about-successful-man-svgrepo-com.svg" alt="about" className="md:hidden w-9 h-9 p-1" />
+                    <span className="mr-1 md:px-1">About Me</span>
+                   </a>
+                   <a href="#" className="nav_item" >
+                    <img src="/contact-svgrepo-com.svg" alt="contact" className="md:hidden w-9 h-9 p-1" />
+                    <span className="mr-1 md:px-1">Contact</span>
+                   </a>  
+                
+              </div>
+              <div className="md:hidden w-[calc[100%-32px]] flex items-center justify-start ml-4">
+                  <button  className="my-1 px-5 py-1 font-semibold text-base inline-block rounded-lg bg-blue-400 hover:bg-cyan-200 
                 hover:transition-colors text-white hover:text-black
-                ring-2 ring-offset-2 ring-blue-400">Resume</button>
+                ring-2 ring-offset-2 ring-blue-400"><a href="#">Resume</a></button>
               </div>
-              <div className="flex flex-col md:flex-row items-center justify-center">
-              <li className="md:mx-3"> <a href="#" >Home</a></li>
-                 <li className="md:mx-3"> <a href="#" >Skill</a></li>
-                 <li className="md:mx-3"> <a href="#" >Services</a></li>
+              
+              <a href="#" className="md:hidden font-bold text-orange-800 text-wrap font-mono mx-4 w-[calc(100%-32px)]  hover:text-blue-600 flex items-center justify-start mt-2">
+                <img src="/gmail-svgrepo-com.svg" alt="gmail" className="w-5 h-5 mr-1" />
+                <span>moshfequrrahman57@gmail.com</span>
+                </a>
+                <a href="#" className="md:hidden font-bold text-gray-600 text-wrap font-mono mx-4 w-[calc(100%-32px)]  hover:text-gray-700 flex items-center justify-start mt-2">
+                <img src="/contact-svgrepo-com.svg" alt="gmail" className="w-5 h-5 mr-1" />
+                <span>+88-01744975767</span>
+                </a>
+              <div className="md:hidden mx-4 flex items-center justify-start mb-4 mt-2">
+              <a href="#"> <img src="/github-svgrepo-com.svg" alt="github" className="important_link_design" /></a>
+              <a href="#"> <img src="/facebook-svgrepo-com (1).svg" alt="facebook" className="important_link_design" /></a>
+              <a href="#"> <img src="/linkedin-round-svgrepo-com.svg" alt="linkedIn" className="important_link_design" /></a>
+              <a href="#"> <img src="/instagram-rounded-svgrepo-com.svg" alt="insta" className="important_link_design" /></a>
               </div>
-              <div className="md:hidden"></div>
+              
             </ul>
-            <button className="rounded-full hover:bg-slate-100 md:hover:bg-transparent">
-            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1 mx-1 md:mx-14"/></button>
+            <button className="rounded-full hover:bg-slate-100 md:hover:bg-transparent mx-1">
+            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1  md:mx-14"/></button>
             <button onClick={()=>{
                 console.log("Button Clicked");
                nav.current.classList.toggle("clip-end-dup");
-               document.body.classList.toggle("no-scroll");
+               //document.body.classList.toggle("no-scroll");
             // nav.current.classList.toggle("opacity_full");
 
-            }} className="relative rounded-full hover:bg-slate-100 md:hover:bg-transparent">
-            <img  src="/menu-dots-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1 mx-1 md:mx-14 md:hidden"/></button>
+            }} className="relative rounded-full hover:bg-slate-100 md:hover:bg-transparent mx-1 md:hidden">
+            <img  src="/menu-dots-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1  md:mx-14 "/></button>
        
             </div>
             </header>
