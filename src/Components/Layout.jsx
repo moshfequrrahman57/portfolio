ce40135cda76129c63a1a58017289fa7c344a873
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-16 14:53:52 +0600

    navbar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -2,49 +2,50 @@ import { useRef } from "react";
 
 const Layout = (props) => {
     const nav=useRef();
-     const transparent_div=useRef();
+    // const transparent_div=useRef();
 
   
     return (
       
         <div  className="m-0 p-0 box-border">
-        <header className=" shadow w-full fixed top-0 lef flex items-center justify-between bg-green-500 z-30">
+        <header className=" shadow-lg m-3 w-[calc(100%-24px)] fixed top-0 left-0 rounded-lg border
+        flex items-center justify-between bg-gradient-to-r from-slate-400 to-[#FBF9AC] z-30">
             <figure className="">
                 <img src="/signature-solid-svgrepo-com.svg" alt="logo" width={50} className="w-14 h-14 p-1"/>
             </figure>
-            <div className="flex items-center justify-end ">
-                <ul ref={nav} className="absolute md:relative top-14 md:top-0 left-0 m-10 md:m-0 w-[calc(100%-80px)] h-[calc(100vh-56px-80px)] md:w-full md:h-fit
-                rounded-xl shadow-lg md:rounded-none md:shadow-none 
-                flex-col md:flex-row   items-center  bg-yellow-200 justify-around md:justify-end 
-               flex  clip-start md:[clip-path:none] opacity-0 md:opacity-100 
-               transition-all duration-[2000ms] ease-in-out z-40">
-               <li> <a href="#" >Home</a></li>
-                 <li> <a href="#" >Skill</a></li>
-                 <li> <a href="#" >Services</a></li>
+            <div className="flex md:w-1/2 items-center justify-around">
+                <ul ref={nav} className="absolute md:relative top-[68px] md:top-0 right-0 
+                md:m-0 w-[calc(100%-80px)] min-h-[calc(100vh-80px)] md:min-h-fit md:w-full md:h-fit
+                rounded-t-lg shadow-sm md:rounded-none md:shadow-none 
+                flex-col md:flex-row   md:items-center  bg-gradient-to-l from-[#FBF9AC] to-slate-300 
+                justify-start md:bg-none 
+               flex  clip-start md:[clip-path:none] 
+               transition-all duration-[2000ms] md:duration-0 ease-in-out z-40">
+              <div className="md:hidden w-full flex items-center justify-around">
+                  <button  className="my-3 px-5 py-2 font-semibold text-lg inline-block rounded-lg bg-blue-400 hover:bg-cyan-200 
+                hover:transition-colors text-white hover:text-black
+                ring-2 ring-offset-2 ring-blue-400">Resume</button>
+              </div>
+              <div className="flex flex-col md:flex-row items-center justify-center">
+              <li className="md:mx-3"> <a href="#" >Home</a></li>
+                 <li className="md:mx-3"> <a href="#" >Skill</a></li>
+                 <li className="md:mx-3"> <a href="#" >Services</a></li>
+              </div>
+              <div className="md:hidden"></div>
             </ul>
-            <button className="">
-            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1"/></button>
+            <button className="rounded-full hover:bg-slate-100 md:hover:bg-transparent">
+            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1 mx-1 md:mx-14"/></button>
             <button onClick={()=>{
                 console.log("Button Clicked");
                nav.current.classList.toggle("clip-end-dup");
                document.body.classList.toggle("no-scroll");
-               nav.current.classList.toggle("opacity_full");
+            // nav.current.classList.toggle("opacity_full");
 
-
-               transparent_div.current.classList.toggle("active_dropdown");
-               let value=document.body.scrollHeight;
-               console.log("Value: ",value);
-               let value_string=value.toString();
-               value_string=value_string.concat("px");
-              transparent_div.current.style.height=value_string;
-             
-              
-            }} className="relative">
-            <img  src="/menu-dots-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1 md:hidden"/></button>
+            }} className="relative rounded-full hover:bg-slate-100 md:hover:bg-transparent">
+            <img  src="/menu-dots-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1 mx-1 md:mx-14 md:hidden"/></button>
        
             </div>
             </header>
-            <div ref={transparent_div} className="absolute hidden top-0 left-0 w-full h-16  bg-emerald-300 z-10">abcdefgh</div>
         {props.children}
         </div>
      
--- src/Pages/Homepages.jsx
+++ src/Pages/Homepages.jsx
@@ -6,7 +6,8 @@ const Homepages = () => {
  
     return (
        <Layout>
-        <p className=' w-full h-[300vh] flex items-center bg-red-200 justify-center text-3xl font-bold'>This is Home page</p>
+        <p className=' w-full h-[300vh] flex items-center  justify-center
+         text-3xl font-bold scroll-m-0'>This is Home page</p>
        </Layout>
     );
 };
