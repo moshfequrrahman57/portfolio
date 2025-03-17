8451169b2d0b07cd4dc76e105108cd314b6a3987
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-17 14:00:25 +0600

    night navbar

--- src/Components/Layout.jsx
+++ src/Components/Layout.jsx
@@ -9,15 +9,15 @@ const Layout = (props) => {
       
         <div  className="m-0 p-0 box-border">
         <header className=" shadow-lg m-3 w-[calc(100%-24px)] fixed top-0 left-0 rounded-lg border
-        flex items-center justify-between bg-gradient-to-r from-slate-400 to-[#FBF9AC] z-30">
+        flex items-center justify-between bg-gradient-to-r from-slate-400 dark:from-slate-900 to-orange-100 z-30 dark:to-slate-400">
             <figure className="">
-                <img src="/signature-solid-svgrepo-com.svg" alt="logo" width={50} className="w-14 h-14 p-1"/>
+                <img src="/signature-solid-svgrepo-com.svg" alt="logo" width={50} className="w-14 h-14 p-1 dark:filter_color_semi_white"/>
             </figure>
             <div className="flex md:w-3/4 items-center justify-around">
                 <ul ref={nav} className="absolute md:relative top-[68px] md:top-0 right-0 
                 md:m-0 w-full h-[calc(100vh-80px)] md:w-full md:h-fit
                 rounded-t-lg shadow-lg md:rounded-none md:shadow-none 
-                flex-col md:flex-row   md:items-center  bg-gradient-to-l from-[#FBF9AC] to-slate-300 
+                flex-col md:flex-row   md:items-center  bg-gradient-to-r from-slate-400 dark:from-slate-900 to-orange-100 dark:to-slate-400 
                 justify-between md:bg-none overflow-auto md:overflow-hidden
                flex  clip-start md:[clip-path:none] 
                transition-all duration-[1000ms] md:duration-0 ease-in-out z-40">
@@ -26,28 +26,28 @@ const Layout = (props) => {
               </div>
               <div className="flex flex-col md:flex-row items-center justify-center m-4 pb-3 md:pb-0 md:m-0 border-b-2 border-slate-500 md:border-none">
                    <a href="#" className="nav_item" >
-                    <img src="/home-svgrepo-com.svg" alt="home" className="md:hidden w-9 h-9 p-1" />
-                    <span className="mr-1 md:px-1">Home</span>
+                    <img src="/home-svgrepo-com.svg" alt="home" className="ul_svg" />
+                    <span className="ml-2 md:px-1 dark:text-white">Home</span>
                    </a>
                    <a href="#" className="nav_item" >
-                    <img src="/skill.svg" alt="skill" className="md:hidden w-9 h-9 p-1" />
-                    <span className="mr-1 md:px-1">Skill</span>
+                    <img src="/skill.svg" alt="skill" className="ul_svg" />
+                    <span className="ml-2 md:px-1 dark:text-white">Skill</span>
                    </a>
                    <a href="#" className="nav_item" >
-                    <img src="/services.svg" alt="services" className="md:hidden w-9 h-9 p-1" />
-                    <span className="mr-1 md:px-1">Services</span>
+                    <img src="/services.svg" alt="services" className="ul_svg" />
+                    <span className="ml-2 md:px-1 dark:text-white">Services</span>
                    </a>
                    <a href="#" className="nav_item" >
-                    <img src="/project-21.svg" alt="projects" className="md:hidden w-9 h-9 p-1" />
-                    <span className="mr-1 md:px-1">Projects</span>
+                    <img src="/project-21.svg" alt="projects" className="ul_svg" />
+                    <span className="ml-2 md:px-1 dark:text-white">Projects</span>
                    </a>
                    <a href="#" className="nav_item" >
-                    <img src="/about-successful-man-svgrepo-com.svg" alt="about" className="md:hidden w-9 h-9 p-1" />
-                    <span className="mr-1 md:px-1">About Me</span>
+                    <img src="/about-successful-man-svgrepo-com.svg" alt="about" className="ul_svg" />
+                    <span className="ml-2 md:px-1 dark:text-white">About Me</span>
                    </a>
                    <a href="#" className="nav_item" >
-                    <img src="/contact-svgrepo-com.svg" alt="contact" className="md:hidden w-9 h-9 p-1" />
-                    <span className="mr-1 md:px-1">Contact</span>
+                    <img src="/contact-svgrepo-com.svg" alt="contact" className="ul_svg" />
+                    <span className="ml-2 md:px-1 dark:text-white">Contact</span>
                    </a>  
                 
               </div>
@@ -57,12 +57,14 @@ const Layout = (props) => {
                 ring-2 ring-offset-2 ring-blue-400"><a href="#">Resume</a></button>
               </div>
               
-              <a href="#" className="md:hidden font-bold text-orange-800 text-wrap font-mono mx-4 w-[calc(100%-32px)]  hover:text-blue-600 flex items-center justify-start mt-3">
-                <img src="/gmail-svgrepo-com.svg" alt="gmail" className="w-5 h-5 mr-1" />
+              <a href="#" className="md:hidden font-bold text-orange-800 dark:text-white text-wrap font-mono mx-4 w-[calc(100%-32px)] 
+               hover:text-blue-600 dark:hover:text-red-100 flex items-center justify-start mt-3">
+                <img src="/gmail-svgrepo-com.svg" alt="gmail" className="w-5 h-5 mr-2 dark:filter_color_blue" />
                 <span>moshfequrrahman57@gmail.com</span>
                 </a>
-                <a href="#" className="md:hidden font-bold text-gray-600 text-wrap font-mono mx-4 w-[calc(100%-32px)]  hover:text-gray-700 flex items-center justify-start mt-3">
-                <img src="/contact-svgrepo-com.svg" alt="gmail" className="w-5 h-5 mr-1" />
+                <a href="#" className="md:hidden font-bold text-gray-600 dark:text-slate-300 text-wrap font-mono mx-4 w-[calc(100%-32px)] 
+                 hover:text-gray-700 dark:hover:text-red-100 flex items-center justify-start mt-3">
+                <img src="/contact-svgrepo-com.svg" alt="gmail" className="w-5 h-5 mr-2 dark:filter_color_blue" />
                 <span>+88-01744975767</span>
                 </a>
               <div className="md:hidden mx-4 flex items-center justify-start mb-3 mt-3">
@@ -73,12 +75,12 @@ const Layout = (props) => {
               </div>
               
             </ul>
-            <button className="rounded-full hover:bg-slate-100 md:hover:bg-transparent mx-1">
-            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1  md:mx-14"/></button>
+            <button className="rounded-full hover:bg-slate-100 dark:hover:bg-emerald-200  mx-1 md:mx-14">
+            <img src="/light-mode-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1"/></button>
             <button onClick={()=>{
                 console.log("Button Clicked");
                nav.current.classList.toggle("clip-end-dup");
-            }} className="relative rounded-full hover:bg-slate-100 md:hover:bg-transparent mx-1 md:hidden">
+            }} className="relative rounded-full  hover:bg-slate-100 dark:hover:bg-emerald-200 md:hover:bg-transparent mx-1 md:hidden">
             <img  src="/menu-dots-svgrepo-com.svg" alt="dark" width={40} height={40} className="p-1  md:mx-14 "/></button>
        
             </div>
