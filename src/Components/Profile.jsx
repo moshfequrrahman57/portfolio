21a3f531b6adfd114e02076228973db457cbe277
Moshfequr <moshfequrrahman57@gmail.com>
2025-03-19 08:22:03 +0600

    profile component almost done

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,17 +2,39 @@ import React from 'react';
 
 const Profile = () => {
     return (
-        <div className='mt-24 mx-3  w-[calc(100%-24px]'>
-            <div className='flex items-center'>
+        <div className='mx-5  w-[calc(100%-40px] h-screen flex flex-col items-start md:items-center justify-start mt-24 md:mt-0  md:justify-center'>
+            <div className='hidden gap-8 items-center'>
                 <img src="/hand-with-fingers-splayed-svgrepo-com.svg" alt="Hi" className='w-6 md:w-7 h-6 md:h-7 mr-1 float-left' />
                 <span className='text-2xl font-semibold'> My name is</span>
+                
             </div>
-            <p className='text-4xl md:text-6xl font-extrabold mt-3'>Md. Moshfequr Rahman.</p>
+            <p className='tracking-wider text-4xl md:text-6xl font-extrabold webkit-text-outline dark:webkit-text-outline-dark'>Md. Moshfequr Rahman.</p>
             <p className='text-3xl md:text-5xl font-bold mt-2'>Frontend develper</p>
-            <div>
-            <p className='text-2xl md:text-2xl font-bold mt-2'>Expert in React - Tailwindcss</p>
-             
+            
+            <div className='text-2xl md:text-2xl font-bold mt-2'>
+                <p>Expert in <img src="/react-svgrepo-com.svg" alt="react" className='w-8 h-8 inline-block' /> React  <span> & </span>
+                 <img src="/tailwind-svgrepo-com.svg" alt="react" className='w-8 h-8 inline-block' /> Tailwindcss </p>
             </div>
+            <div className="flex items-center justify-start mt-5">
+              <a href="#"> <img src="/github-svgrepo-com.svg" alt="github" className="important_link_in_profile" /></a>
+              <a href="#"> <img src="/facebook-svgrepo-com (1).svg" alt="facebook" className="important_link_in_profile" /></a>
+              <a href="#"> <img src="/linkedin-round-svgrepo-com.svg" alt="linkedIn" className="important_link_in_profile" /></a>
+              <a href="#"> <img src="/instagram-rounded-svgrepo-com.svg" alt="insta" className="important_link_in_profile" /></a>
+              </div>
+              <div className='flex gap-10 mt-5 md:mt-8'>
+              <div className="flex items-center justify-start">
+                  <button  className="my-1 px-5 py-2 font-semibold text-base inline-block rounded-lg bg-blue-400 hover:bg-cyan-200 
+                hover:transition-colors text-white hover:text-black
+                ring-2 ring-offset-2 ring-blue-400"><a href="#">Resume</a></button>
+              </div>
+              <div className="flex items-center justify-start ">
+                  <button  className="my-1 px-5 py-2 font-semibold text-base inline-block rounded-lg bg-blue-400 hover:bg-cyan-200 
+                hover:transition-colors text-white hover:text-black
+                ring-2 ring-offset-2 ring-blue-400"><a href="#">My projects</a></button>
+              </div>
+              </div>
+             
+            
         </div>
     );
 };
